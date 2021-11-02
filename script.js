01d3ff59e25dad761b1037cea9fae0d837ab1f9a
phillipsalangsang <phillip_salangsang@hotmail.com>
2021-11-02 09:26:14 +0000

    Changes to the js

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
 "use strict"
 
+// function split4(pointerEvent){
+//     if(!pointerEvent.shiftKey){
 
-function split4(){
-    //splits a dit into 4 by adding 4 quarters
-    addQuarter(this)
-    addQuarter(this)
-    addQuarter(this)
-    addQuarter(this)
+//     //splits a dit into 4 by adding 4 quarters
+//     addQuarter(this)
+//     addQuarter(this)
+//     addQuarter(this)
+//     addQuarter(this)
+//     }else{
+//         deleteQuarters(this)
+//     }    
+// }
 
-    //Challenge - Set the background colour of each div generated to a random colour
-    
-    ///rgb() function
-    //Math.random
+function split4(e){
 
+    if(e.shiftKey){
+        //shift is pressed - lets all the children of my parent (that includes me!)
+        this.parentNode.addEventListener("click",split4)
+        this.parentNode.innerHTML=''  //or delete the children a nicer way
+    }
+    else{
+        //Split a div into 4 by adding 4 quarters
 
+       // this.removeEventListener("click",split4)
+        addQuarter(this)
+        addQuarter(this)
+        addQuarter(this)
+        addQuarter(this)
+    }
 
+    // Event 'capturing and bubbling' 
+    e.stopPropagation() //stop the event from firing again on all the ancestor divs
 }
+
+// function rgbGenerator(){
+//     let r = Math.floor(Math.random()*255)
+//     let g = Math.floor(Math.random()*255)
+//     let b = Math.floor(Math.random()*255)
+//     return `rgb(${r}, ${g}, ${b})`
+// }
 
 
 function addQuarter(intoDiv){
     
-    
-    
     let q = document.createElement("div") //create a 'div' element - and hold a 'pointer' to it in the variable 'q'
-
-
-
     q.style.backgroundColor=`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
 
-    q.style.backgroundColor = "red"
     //add the two classes 'quarter' and 'flexy' to this new element
     q.classList.add("flexy")
     q.classList.add("quarter")
